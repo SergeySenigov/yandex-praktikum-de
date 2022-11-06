784a87495b9eafb73f6b161997b5c75a73777e2a
SergeySenigov <ssbox@yahoo.com>
2022-11-06 08:17:24 +0500

    ggg

--- docker/simple_dockerfile/app.py
+++ docker/simple_dockerfile/app.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
-import requests
-import pandas as pd
-from tabulate import tabulate
+#import requests
+#import pandas as pd
+#from tabulate import tabulate
+import platform
 
-def fetch():
-	status = requests.get("https://www.githubstatus.com/api/v2/summary.json").json()['components']
-	df = pd.DataFrame(status)[['name', 'status']]
-	print(tabulate(df, headers='keys', tablefmt='fancy_grid'))
+#def fetch():
+#	status = requests.get("https://www.githubstatus.com/api/v2/summary.json").json()['components']
+#	df = pd.DataFrame(status)[['name', 'status']]
+#	print(tabulate(df, headers='keys', tablefmt='fancy_grid'))
+
+def print_version():
+	print(platform.version())
+	print(platform.system())
+	print(platform.release())
+	print(platform.platform())
+	print(platform.mac_ver())
 
 if __name__ == "__main__":
-	fetch()
+#	fetch()
+	print_version()
 
 	
 	
